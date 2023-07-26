c2fdb73f7d452a52267879d8b337e0f1fe057bb5
minikryc <mini122600@gmail.com>
2023-07-26 19:46:59 +0900

    Initial commit

---
+++ src/test/java/com/bteam/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bteam.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
